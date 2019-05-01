7fee59dc79d45eafb0dc68ff2abbfe1b8945f820
FranChan <mrfancyfrancis@gmail.com>
2019-05-01 19:53:39 +0800

    Working CSV Import (untested)

--- JDL/src/jdl/controller/CSVLoaderMain.java
+++ JDL/src/jdl/controller/CSVLoaderMain.java
@@ -19,9 +19,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import au.com.bytecode.opencsv.CSVReader;
 import jdl.dao.CSVQueries;
+import jdl.dao.Queries;
 import jdl.model.Transaction;
 
 public class CSVLoaderMain {
@@ -58,10 +60,12 @@ public class CSVLoaderMain {
 			Files.write(path, content.getBytes(charset));
 			String[] r = content.split("\n");
 			String[] columns = r[0].split(",");
+			columns[columns.length-1] = columns[columns.length-1].trim();
 			String[][] rows = new String[r.length-1][columns.length];
 			for(int i = 0; i < rows.length;i++)
 			{
 				rows[i] = r[i+1].split(",");
+				rows[i][rows[i].length-1] = rows[i][rows[i].length-1].trim();
 			}
 			for(String[] ss: rows)
 			{
@@ -71,10 +75,48 @@ public class CSVLoaderMain {
 				}
 				System.out.println();
 			}
-			//System.out.println(CSVQueries.checkColumns(columns));
+			//System.out.println(checkColumns(columns));
 			if(checkColumns(columns))
 			{
-				
+				int c = 0;
+				for(String[] ss: rows)
+				{
+					//System.out.println(ss[0]+" "+checkInput(ss)); 
+					boolean status = false;
+					Transaction t = new Transaction();
+					if(checkInput(ss))
+					{
+						t.setPassportNo(ss[0]);
+						t.setTinID(ss[1]);
+						t.setVisaType(ss[2]);
+						t.setVisaStartDate(new java.sql.Date((new SimpleDateFormat("yyyy-MM-dd").parse(ss[3]).getTime())));
+						t.setVisaEndDate(new java.sql.Date((new SimpleDateFormat("yyyy-MM-dd").parse(ss[4]).getTime())));
+						t.setPermitType(ss[5]);
+						t.setPermitStartDate(new java.sql.Date((new SimpleDateFormat("yyyy-MM-dd").parse(ss[6]).getTime())));
+						t.setPermitEndDate(new java.sql.Date((new SimpleDateFormat("yyyy-MM-dd").parse(ss[7]).getTime())));
+						t.setAepID(ss[8]);
+						t.setAepStartDate(new java.sql.Date((new SimpleDateFormat("yyyy-MM-dd").parse(ss[9]).getTime())));
+						t.setAepEndDate(new java.sql.Date((new SimpleDateFormat("yyyy-MM-dd").parse(ss[10]).getTime())));
+						t.setTransID(Integer.parseInt(ss[11]));
+						t.setClient_id(Integer.parseInt(ss[12]));
+						t.setTransTimestamp(new java.sql.Date((new SimpleDateFormat("yyyy-MM-dd").parse(ss[13]).getTime())));
+						t.setTransAuthor(ss[14]);
+						status = Queries.insertTransaction(t);
+					}
+					else
+					{
+						status = false;
+					}
+					if(status)
+						JOptionPane.showMessageDialog(null, "<html><font color = #ffffff>Transaction Data import from path "+directory+" row["+Integer.toString(c)+" has been successful</font color = #ffffff></html>", "Insert Successful", JOptionPane.INFORMATION_MESSAGE);
+					else
+						JOptionPane.showMessageDialog(null, "<html><font color = #ffffff>Transaction Data import on row["+Integer.toString(c)+" has failed</font color = #ffffff></html>", "Insert Unsuccessful", JOptionPane.INFORMATION_MESSAGE);
+					c++;
+				}
+			}
+			else
+			{
+				JOptionPane.showMessageDialog(null, "<html><font color = #ffffff>Transaction Data import from path "+directory+" has incorrect column names (Check row 1 for correct corresponding row)</font color = #ffffff></html>", "Insert Unsuccessful", JOptionPane.INFORMATION_MESSAGE);
 			}
 			
 			//CSVLoader loader = new CSVLoader(conn1);
@@ -97,10 +139,31 @@ public class CSVLoaderMain {
 		}
 		return true;
 	}
-	public static Transaction addtoModel(String[] c)
+	public static boolean checkInput(String[] c)
 	{
-		Transaction t = new Transaction();
-		
-		return t;
+		try {
+			String vs = c[3];//[index 3]
+			String ve = c[4];//[index 4]
+			String ps = c[5];//[index 6]
+			String pe = c[6];//[index 7]
+			String as = c[7];//[index 9]
+			String ae  = c[8];//[index 10]
+			if(objectFilter.inputCheck("Passport No.",c[0])){
+				if(objectFilter.inputCheck("TIN ID",c[1])) {
+					if(objectFilter.dateCheckTransaction("Visa", c[2], ve, vs)) {
+						if(objectFilter.dateCheckTransaction("AEP", c[8], as, ae)) {
+							if(objectFilter.dateCheckTransaction("Permit", c[5], ps, pe)) 
+							{
+								return true;
+							}
+						}
+					}
+				}
+			}			
+			
+		}catch (Exception e3) {
+			e3.printStackTrace();
+		}
+		return false;
 	}
 }
