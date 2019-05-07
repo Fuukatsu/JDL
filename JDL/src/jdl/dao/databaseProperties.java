66ebfc75439fd38ce8044f4ea8026b2f8751a8fc
Fuukatsu <mr.andrewmoore04@gmail.com>
2019-05-07 10:19:08 +0800

    Change database properties

--- JDL/src/jdl/dao/databaseProperties.java
+++ JDL/src/jdl/dao/databaseProperties.java
@@ -3,7 +3,7 @@ package jdl.dao;
 
 public class databaseProperties 
 {
-	public String url = "jdbc:mysql://192.168.254.103:3306/jdl_accounts?autoReconnect=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	public String url = "jdbc:mysql://192.168.1.11:3306/jdl_accounts?autoReconnect=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	public String username = "root";
 	public String password = "password";
 }
